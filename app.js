2b52bb0538e8abb95edda60234b539ba7ad04d2d
DianaBul <97395094+DianaBul@users.noreply.github.com>
2022-01-09 21:34:49 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const testMarks = [{
+        name: 'Vasya',
+        avgMark: 3.75
+    }, {
+        name: 'Lena',
+        avgMark: 4.89
+    }];
+function getStatistics(marks) {
+    let averageMark;
+    let avgMarkSum = 0;
+    const len = testMarks.length;
+    let iavg = null;
+    let iname = null;
+    for (let i = 0; i < len; i++) {
+        iavg = testMarks[i];
+        if (iavg.avgMark) {
+            avgMarkSum = iavg.avgMark + avgMarkSum;
+            averageMark = avgMarkSum / 2;
+        }
+    }
+    const avg = {
+        avgMark: averageMark.toString(),
+        HighestMark: 'Lena',
+        lowestMark: 'Vasya'
+    };
+    return avg;
+}
+console.log(getStatistics(testMarks));
+//# sourceMappingURL=app.js.map
